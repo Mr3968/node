8ff229720ef66608e31bc962edccbda8d8c535a3
marui <1150609490@qq.com>
2021-09-01 19:08:27 +0800

    add

---
+++ leecode/2.js
@@ -0,0 +1,27 @@
+// 借助全局变量
+// let func = (function () {
+//   // TODO 请实现对应代码逻辑，每调用一次返回值都会加1
+//   if (!window.count) {
+//     window.count = 0;
+//   }
+//   return () => {
+//     window.count += 1;
+//     return window.count;
+//   };
+// })();
+
+// console.log(func()); // -> 1
+// console.log(func()); // -> 2
+// console.log(func()); // -> 3
+
+let func = (function () {
+  // TODO 请实现对应代码逻辑，每调用一次返回值都会加1
+  let num = 0;
+  return () => {
+    return ++num;
+  };
+})();
+
+console.log(func()); // -> 1
+console.log(func()); // -> 2
+console.log(func()); // -> 3
